6f5c1cb492e6083989862645dd5e208fe7d43ba9
Gazaunga <Gazaunga@gmail.com>
2017-08-13 14:18:39 -0400

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -159,3 +159,11 @@ nil are ignored."
     modified-found
     )
   )
+
+;; define function to shutdown emacs server instance
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server"
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs)
+  )
